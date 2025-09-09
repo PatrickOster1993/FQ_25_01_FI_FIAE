02dc1aabb933b1be7ce30b0f7ceab84703248d93
Patrick Oster <p.oster9330@googlemail.com>
2025-09-09 11:04:46 +0200

    added aufgabe10

--- 01_crashkurs_python_prozedural/aufgaben/aufgabe10.py
+++ 01_crashkurs_python_prozedural/aufgaben/aufgabe10.py
@@ -15,3 +15,51 @@ for i in range(0, anz_kurse):
     kurse.append(kurs)
 
 print(kurse)
+
+# 2
+## A
+avg = sum(kurse) / len(kurse)
+print(f"Durchschnittlicher Kurs: {avg} €")
+
+## B
+max_kurs = max(kurse)
+min_kurs = min(kurse)
+print(f"Min-Kurs: {min_kurs} € | Max-Kurs: {max_kurs} €")
+
+## C
+def rise_or_fall_count(rise = True):
+    tage_veraendert = 0
+    for i in range(1, anz_kurse):
+        vorher = kurse[i - 1]
+        jetzt = kurse[i]
+        if rise and jetzt > vorher:
+            tage_veraendert += 1
+        elif not rise and jetzt < vorher:
+            tage_veraendert += 1
+    return tage_veraendert
+    
+print(f"Anzahl Tage, an denen Kurs gestiegen: {rise_or_fall_count()}")
+
+## D
+### DRY: Don't Repeat Yourself!!!
+print(f"Anzahl Tage, an denen Kurs gefallen: {rise_or_fall_count(False)}")
+
+# 3 Tag, an dem Kurs am höchsten gestiegen:
+
+max_differenz = 0
+max_tag = ""
+
+for i in range(1, anz_kurse):
+    differenz = kurse[i] - kurse[i - 1]
+    if differenz > max_differenz:
+        max_differenz = differenz
+        max_tag = f"Kurs von Tag {i} auf Tag {i + 1} am stärksten gestiegen!"
+
+print(max_tag)
+
+# 4
+performance = round(((kurse[-1] - kurse[0]) / kurse[0]) * 100, 2)
+fall_or_rise = "gestiegen" if performance >= 0 else "gefallen"
+
+print(f"Kurs ist über den Monat hinweg um {abs(performance)} % {fall_or_rise}!")
+
