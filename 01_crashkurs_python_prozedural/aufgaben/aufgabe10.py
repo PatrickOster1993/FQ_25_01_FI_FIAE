2c5118a422d5bd89598443438c155ddb9eb0d855
Florian Koch <floko888@gmail.com>
2025-09-09 10:25:30 +0200

    added aufgabe10 subtasks (feel free to improve)

--- 01_crashkurs_python_prozedural/aufgaben/aufgabe10.py
+++ 01_crashkurs_python_prozedural/aufgaben/aufgabe10.py
@@ -15,3 +15,45 @@ for i in range(0, anz_kurse):
     kurse.append(kurs)
 
 print(kurse)
+
+# 2. Ausgabe berechnen:
+
+## A. Den durchschnittlichen Aktienkurs des Monats.
+
+### medium Rare Murat
+print(sum(kurse)/len(kurse))
+
+## B. Den höchsten und niedrigsten Kurs.
+
+def print_extrama():
+    print("Max:", max(kurse))
+    print("Min:", min(kurse))
+print_extrama()
+
+## C. Die Anzahl der Tage, an denen der Kurs gestiegen ist.
+
+def count_previous_element_is_bigger(elements):
+    
+    count_of_elements_that_are_bigger = 0
+    
+    for i in range(1, len(elements)):
+        if elements[i] > elements[i-1]:
+            count_of_elements_that_are_bigger += 1;
+            
+    return count_of_elements_that_are_bigger;
+
+print("Tage an denen der Kurs gestiegen ist:", count_previous_element_is_bigger(kurse))
+
+## D. Die Anzahl der Tage, an denen der Kurs gefallen ist
+
+def count_previous_element_is_smaller(elements):
+    
+    count_of_elements_that_are_smaller = 0
+    
+    for i in range(1, len(elements)):
+        if elements[i] < elements[i-1]:
+            count_of_elements_that_are_smaller += 1;
+            
+    return count_of_elements_that_are_smaller;
+
+print("Tage an denen der Kurs gefallen ist:", count_previous_element_is_smaller(kurse))
