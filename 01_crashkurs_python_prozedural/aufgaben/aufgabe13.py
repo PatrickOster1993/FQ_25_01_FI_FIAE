b1fd4d8ece66455f3e72096d6badddf4aba6a4c9
Patrick Oster <p.oster9330@googlemail.com>
2025-09-10 12:35:17 +0200

    finished aufgabe13

--- 01_crashkurs_python_prozedural/aufgaben/aufgabe13.py
+++ 01_crashkurs_python_prozedural/aufgaben/aufgabe13.py
@@ -1,11 +1,12 @@
 # Tic-Tac-Toe
 
 board = []
-playing = True
 player1 = True
 
 def init_board():
     # 3 x 3 Matrix (Tabelle)
+    global board
+    board = []
     for m in range(0, 4):
         temp = []
         for n in range (0, 4):
@@ -26,112 +27,99 @@ def display_board():
             print(element, end=" ")
         print()
 
-def has_hor_winner(symbol, row, col):
-    ## [1][1] and [1][2] and [1][3]
-    ## [2][1] and [2][2] and [2][3]
-    ## [3][1] and [3][2] and [3][3]
-    ## zusammengefasst: 1 innere Liste muss von Index 1 - 3 gleiche Zeichen enthalten (X oder O)
-    return False # True, wenn Sieger!
+def has_winner(symbol):
+    def has_hor_winner(symbol):
+        ## [1][1] and [1][2] and [1][3]
+        ## [2][1] and [2][2] and [2][3]
+        ## [3][1] and [3][2] and [3][3]
+        ## zusammengefasst: 1 innere Liste muss von Index 1 - 3 gleiche Zeichen enthalten (X oder O)
+        for liste in board:
+            if liste[1] == symbol and liste[2] == symbol and liste[3] == symbol:
+                return True
+        return False
 
-def has_ver_winner(symbol, row, col):
-    ## [1][1] and [2][1] and [3][1]
-    ## [1][2] and [2][2] and [3][2]
-    ## [1][3] and [2][3] and [3][3]
-    ## zusammengefasst: wenn für jede innere Liste, beim gleichen Index das gleiche Zeichen gesetzt wurde(X oder O)
-    return False # True, wenn Sieger!
+    def has_ver_winner(symbol):
+        ## [1][1] and [2][1] and [3][1]
+        ## [1][2] and [2][2] and [3][2]
+        ## [1][3] and [2][3] and [3][3]
+        ## zusammengefasst: wenn für jede innere Liste, beim gleichen Index das gleiche Zeichen gesetzt wurde(X oder O)
+        counter_list = [0, 0, 0]
+        for m in range(0, 4):
+            if board[m][1] == symbol:
+                counter_list[0] += 1
+            elif board[m][2] == symbol:
+                counter_list[1] += 1
+            elif board[m][3] == symbol:
+                counter_list[2] += 1
+        if 3 in counter_list:
+            return True
+        return False
 
-def has_dia_winner(symbol, row, col):
-    ## [1][1] and [2][2] and [3][3]
-    ## [1][3] and [2][2] and [3][1]
-    ## zusammengefasst: [2][2] muss immer mit gleichem Zeichen (X oder O) versehen sein UND gesonderte Teilbedingungen
-    return False # True, wenn Sieger!
+    def has_dia_winner(symbol):
+        ## [1][1] and [2][2] and [3][3]
+        ## [1][3] and [2][2] and [3][1]
+        ## zusammengefasst: [2][2] muss immer mit gleichem Zeichen (X oder O) versehen sein UND gesonderte Teilbedingungen
+        if board[2][2] == symbol:
+            if board[1][1] == symbol and board[3][3] == symbol:
+                return True
+            elif board[1][3] == symbol and board[3][1] == symbol:
+                return True
+        return False # True, wenn Sieger!
+    
+    return has_hor_winner(symbol) or has_ver_winner(symbol) or has_dia_winner(symbol)
+
+def is_ocupied():
+    for liste in board:
+        if "." in liste:
+            return False
+    print("Spielfeld vollständig belegt - Remis!")
+    print()
+    return True
 
 init_board()
 display_board()
 
-while(playing):
+while(True):
+
     if player1:
-        print("Spieler 1 (X) am Zug:")
-        print("---------------------")
-        try:
-            row = int(input("Zeile eingeben (1-3): "))
-            if row < 1 or row > 3:
-                print("Bitte eine Zahl zwischen 1-3 eingeben!")
-                continue
-        except:
-            print("Bitte eine Zahl zwischen 1-3 eingeben!")
-            continue
-        
-        try:
-            col = int(input("Spalte eingeben (1-3): "))
-            if col < 1 or col > 3:
-                print("Bitte eine Zahl zwischen 1-3 eingeben!")
-                continue
-        except:
-            print("Bitte eine Zahl zwischen 1-3 eingeben!")
-            continue
-        print()
-        if board[row][col] == ".":
-            board[row][col] = "X"
-            display_board()
-        else:
-            print("Feld bereits belegt!")
-        if has_hor_winner("X", row, col) or has_ver_winner("X", row, col) or has_dia_winner("X", row, col):
-            print("Glückwunsch, Spieler 1... du hast gewonnen!")
-            break
-        player1 = False
+        symbol = "X"
+        player = 1
     else:
-        print("Spieler 2 (O) am Zug:")
-        print("---------------------")
-        try:
-            row = int(input("Zeile eingeben (1-3): "))
-            if row < 1 or row > 3:
-                print("Bitte eine Zahl zwischen 1-3 eingeben!")
-                continue
-        except:
+        symbol = "O"
+        player = 2
+
+    print(f"Spieler {player} ({symbol}) am Zug:")
+    print("---------------------")
+    try:
+        row = int(input("Zeile eingeben (1-3): "))
+        if row < 1 or row > 3:
             print("Bitte eine Zahl zwischen 1-3 eingeben!")
             continue
-        
-        try:
-            col = int(input("Spalte eingeben (1-3): "))
-            if col < 1 or col > 3:
-                print("Bitte eine Zahl zwischen 1-3 eingeben!")
-                continue
-        except:
+    except:
+        print("Bitte eine Zahl zwischen 1-3 eingeben!")
+        continue
+    
+    try:
+        col = int(input("Spalte eingeben (1-3): "))
+        if col < 1 or col > 3:
             print("Bitte eine Zahl zwischen 1-3 eingeben!")
             continue
-        print()
-        if board[row][col] == ".":
-            board[row][col] = "O"
-            display_board()
-        else:
-            print("Feld bereits belegt!")
-        if has_hor_winner("O", row, col) or has_ver_winner("O", row, col) or has_dia_winner("O", row, col):
-            print("Glückwunsch, Spieler 1... du hast gewonnen!")
-            break
+    except:
+        print("Bitte eine Zahl zwischen 1-3 eingeben!")
+        continue
+    print()
+    if board[row][col] == ".":
+        board[row][col] = symbol
+        display_board()
+    else:
+        print("Feld bereits belegt!")
+        player1 = not player1
+    if has_winner(symbol):
+        print(f"Glückwunsch, Spieler {player}... du hast gewonnen!")
+        break
+    if is_ocupied():
+        init_board()
+        display_board()
         player1 = True
-
-# Siegesbedingung bzgl. X oder O:
-# 1. Horizontal: Komplette Zeile voll 
-# 2. Vertikal: Komplette Spalte voll
-# 3. Diagonal: Komplette Diagonale voll
-
-# 1.: 
-## [1][1] and [1][2] and [1][3]
-## [2][1] and [2][2] and [2][3]
-## [3][1] and [3][2] and [3][3]
-## zusammengefasst: 1 innere Liste muss von Index 1 - 3 gleiche Zeichen enthalten (X oder O)
-
-# 2.:
-## [1][1] and [2][1] and [3][1]
-## [1][2] and [2][2] and [3][2]
-## [1][3] and [2][3] and [3][3]
-## zusammengefasst: wenn für jede innere Liste, beim gleichen Index das gleiche Zeichen gesetzt wurde(X oder O)
-
-# 3.:
-## [1][1] and [2][2] and [3][3]
-## [1][3] and [2][2] and [3][1]
-## zusammengefasst: [2][2] muss immer mit gleichem Zeichen (X oder O) versehen sein UND gesonderte Teilbedingungen
-
-
-
+        continue
+    player1 = not player1
